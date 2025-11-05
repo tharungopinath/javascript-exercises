d742adf87f51d103bb728e08ea3f0ffff13ca154
tharun <tharunkaarthikgopinath@gmail.com>
2025-11-04 21:10:29 -0500

    finished exercise 6 in javascript-exercises

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeat = "";
+    if (num >= 0) {
+        for (let i=0; i < num; i++){
+            repeat+=string;
+        }
+    }
+    else {
+        return "ERROR";
+    }
+    return repeat;
 };
 
 // Do not edit below this line
