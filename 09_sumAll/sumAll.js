cc6390541c6cd91de061a65dfc3c57ca7cd716f1
tharun <tharunkaarthikgopinath@gmail.com>
2025-11-05 01:49:35 -0500

    completed exercise 9 in javascript-exercises

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let max;
+    let arr = [];
+    let index = 0;
+    let ans = [];
+    while (Number.isInteger(a) && Number.isInteger(b) && a>0 && b>0){
+        max = (a>b) ? a : b;
+        min = (a>b) ? b : a;    
+        for (let i = min; i <= max; i++) {
+            arr[index] = i;
+            index++;
+        }
+        ans = arr.reduce((total, cur) => {
+            return total + cur;
+        }, 0)
+        return ans;
+    }
+    return 'ERROR';
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
