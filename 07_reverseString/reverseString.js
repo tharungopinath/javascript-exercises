cde19c076fadb13c44ebe422aab86bc0a867a435
tharun <tharunkaarthikgopinath@gmail.com>
2025-11-04 22:06:21 -0500

    completed the 7th javascript exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr = [];
+    let reverse = "";
+    for (let i=0; i < string.length; i++) {
+        arr[i] = string.charAt(string.length - 1 - i);
+        reverse+=arr[i];
+    }
+    return reverse;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
