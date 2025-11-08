831037c61e395de970bb47c9ffc78b748159335e
tharun <tharunkaarthikgopinath@gmail.com>
2025-11-07 20:39:44 -0500

    completed exercise 13 in javascript-exercises

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const str = string.replace(/[" '!,."]/g, "").toLowerCase();
+    const mid = Math.trunc(str.length / 2);
+    for (let i = 0; i < mid; i++){
+         if (str[i] !== str[str.length - 1 - i]) {
+             return false;
+         }
+    }
+    return true;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
