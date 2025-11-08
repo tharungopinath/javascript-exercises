7290df6aac2c4d4596b83408883009e29367620b
tharun <tharunkaarthikgopinath@gmail.com>
2025-11-07 20:07:53 -0500

    completed exercise 12 in javascript-exercises

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(sum) {
+	return sum.reduce((total, current) => total = total+current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(mult) {
+  return mult.reduce((total, current) => total = total * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let fact = 1;
+	for (let i = a; i > 0; i--) {
+    fact *= i; 
+  }
+  return fact;
 };
 
 // Do not edit below this line
