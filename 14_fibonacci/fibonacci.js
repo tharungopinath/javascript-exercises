247e602bf28668dbfe82816642d5fb77a7ddf0d2
tharun <tharunkaarthikgopinath@gmail.com>
2025-11-07 20:49:27 -0500

    completed exercise 14 in javascript exercises

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(term) {
+    let values = [0, 1];
+    let num;
+    if (term == 1 || term == 0){
+        return values[term];
+    }
+    else if (term < 0) {
+        return "OOPS";
+    }
+    else{
+        for (let i = 2; i <= term; i++){
+            num = values[i-1] + values[i-2];
+            values.push(num);
+        }
+        return values[term];
+    }
 };
 
 // Do not edit below this line
